f9c0621f5241a28f8ff58fc94070e08774fb5064
admin <admin@DESKTOP-EDP1GAF>
2022-07-18 14:22:58 +0300

    Добавьте файлы проекта.

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex2
+{
+    class Program
+    {
+        static string wordsSplit(string startedText, char[] separators)
+        {
+            string[] splitedText = startedText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string newText = reverseText(splitedText);
+            return newText;
+        }
+        static string reverseText(string[] text)
+        {
+            Array.Reverse(text);
+            string reversed = string.Join(' ', text);
+            return reversed;
+        }
+        static void Main(string[] args)
+        {
+            char[] separators = new char[] { ' ', '.', ',' };
+            Console.WriteLine("Введите предложение: ");
+            string text = Console.ReadLine();
+
+            text = wordsSplit(text, separators);
+
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex1
+{
+    class Program
+    {
+        static string[] wordsSplit(string text, char[] separators)
+        {
+            string[] splitedText = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return splitedText;
+        }
+        static void Print(string[] splitedText)
+        {
+            for (int i = 0; i < splitedText.Length; i++)
+            {
+                Console.WriteLine($"{splitedText[i]}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            char[] separators = new char[] { ' ', '.', ',' };
+            Console.WriteLine("Введите строку: ");
+            string text = Console.ReadLine();
+
+            wordsSplit(text, separators);
+            Print(wordsSplit(text, separators)); //можно сделать так? или есть другой способ?
+
+            Console.ReadKey();
+        }
+    }
+}
